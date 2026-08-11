1c5eed1b6c4a34a6e31e5838d6f9e0a6edd624ba
Luis de la Peña <lemartinp@unal.edu.co>
2026-08-10 22:50:36 -0500

    fixed index.css

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 import App from './App.tsx'
-import './Index.css'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
